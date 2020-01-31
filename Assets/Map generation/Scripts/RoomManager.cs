7cfb69e97a8c2d3b7980b012750ce3112b261f44
mukmai <deaconkum@ymail.com>
2020-02-01 07:40:39 +0800

    added door and basic room system

---
+++ Assets/Map generation/Scripts/RoomManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class RoomManager : MonoBehaviour
+{
+    public DoorControl[] doors;
+    public GameObject[] enemys;
+    public int enemyCount = 0;
+    private bool roomBegan = false;
+
+    private void Start()
+    {
+        foreach (DoorControl door in doors)
+        {
+            door.room = this;
+        }
+
+        foreach (var enemy in enemys)
+        {
+            
+        }
+
+        enemyCount = enemys.Length;
+    }
+
+    private void Update()
+    {
+        if (roomBegan && enemyCount == 0)
+        {
+            EndRoom();
+        }
+    }
+
+    public void BeginRoom()
+    {
+        foreach (DoorControl door in doors)
+        {
+            door.Lock();
+        }
+
+        roomBegan = true;
+        // TODO: start up enemy
+    }
+
+    public void EnemyDead()
+    {
+        enemyCount--;
+    }
+
+    void EndRoom()
+    {
+        foreach (DoorControl door in doors)
+        {
+            door.Unlock();
+        }
+    }
+}
